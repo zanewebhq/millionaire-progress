bb4de49807382c3fde7576c09c55dc3b9fc0bbb1
Martin Zane <martin@zaneweb.com>
2024-08-17 14:06:28 +0200

    Add currency symbol to net worth field & make form submittable

---
+++ src/constants/currencies.ts
@@ -0,0 +1,25 @@
+const currencies = [
+  {
+    name: 'United States Dollar',
+    value: 'USD',
+    symbol: '$',
+    flag: '🇺🇸',
+    label: '🇺🇸 United States Dollar (USD)',
+  },
+  {
+    name: 'Euro',
+    value: 'EUR',
+    symbol: '€',
+    flag: '🇪🇺',
+    label: '🇪🇺 Euro (EUR)',
+  },
+  {
+    name: 'Great Britain Pound',
+    value: 'GBP',
+    symbol: '£',
+    flag: '🇬🇧',
+    label: '🇬🇧 Great Britain Pound (GBP)',
+  },
+];
+
+export default currencies;
