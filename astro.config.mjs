2153af4f66e04a56309e0cedd359f70320eb7e2c
Martin Zane <martin@zaneweb.com>
2024-08-28 15:36:43 +0200

    Add favicon, robots, and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
-import vercel from "@astrojs/vercel/serverless";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: vercel()
+  adapter: vercel(),
+  site: 'https://millionaireprogress.com',
+  integrations: [sitemap()],
 });
